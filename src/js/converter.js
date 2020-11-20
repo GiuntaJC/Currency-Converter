c6d9a5f948019f83170301d27f3e4075ac7ad116
Joseph Giunta <jgnohack@gmail.com>
2020-11-20 15:51:52 -0800

    add test and placeholder values to try passing

---
+++ src/js/converter.js
@@ -0,0 +1,12 @@
+export default class currencyConverter {
+  constructor(usd, currencyType) {
+    this.usd = usd;
+    this.currencyType = currencyType;
+    this.convertedAmount = 0;
+  }
+
+  static convertCurrency() {
+    this.convertedAmount = 7.53;
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import './js/converter.js';
 
 $("#currencyForm").submit((event) => {
   console.log("foobar");
