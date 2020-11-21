a5a27a5c8670c72b3746d5207bccdf00abc492d9
Joseph Giunta <jgnohack@gmail.com>
2020-11-20 16:33:43 -0800

    modified converter.js to pass method test

--- src/js/converter.js
+++ src/js/converter.js
@@ -1,12 +1,12 @@
-export default class currencyConverter {
+export default class currencyConverter{
   constructor(usd, currencyType) {
     this.usd = usd;
     this.currencyType = currencyType;
     this.convertedAmount = 0;
   }
 
-  static convertCurrency() {
-    this.convertedAmount = 7.53;
+  convertCurrency() {
+    return this.convertedAmount = 7.53;
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './js/converter.js';
-
+import Class from'./js/converter.js';
+Class
 $("#currencyForm").submit((event) => {
   console.log("foobar");
   event.preventDefault();
